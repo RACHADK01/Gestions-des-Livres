f1e09a90058dc0d7ce0b636a0522b85cd9850a53
Rachad <300140175@monboreal.ca>
2024-10-13 23:28:03 -0400

    Mise-à-jour des fichiers classe Program.cs et Roman.cs

--- Program.cs
+++ Program.cs
@@ -8,12 +8,19 @@ internal class Program
         Revue revue2 = new Revue("Science et Vie", "National géography", 127, 65, 2013);
         Revue revue3 = new Revue("Terre Africaine", "Berceau humanitaire", 173, 56, 2005);
 
+        Roman roman1 = new Roman("Plus Malin que le Diable", "Napoleon Hill", 256, "Croissance personnelle");
+        Roman roman2 = new Roman("Chants d'ombres", "Léopold Sédar Senghor", 160, "Récueil de poèmes");
+        Roman roman3 = new Roman("Les Misérables", "Victor Hugo", 1462, "Historique");
+
         List<Livre> livres = new List<Livre>();
         livres.Add(revue1);
         livres.Add(revue2);
         livres.Add(revue3);
+        livres.Add(roman1);
+        livres.Add(roman2);
+        livres.Add(roman3);
 
-        Console.WriteLine("Détails des livres dans la liste :");
+        Console.WriteLine("Voici les détails des différents livres de la liste :");
         foreach (Livre livre in livres)
         {
             livre.AfficherDetails();
---
+++ Roman.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCsharp_Roman
+{
+    internal class Roman : Livre
+    {
+        public string Genre { get; set; }
+
+        public Roman(string titre, string auteur, int nombrePages, string genre) : base(titre, auteur, nombrePages)
+        {
+            Genre = genre;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NombrePages}, Genre: {Genre}");
+        }
+    }
+}
