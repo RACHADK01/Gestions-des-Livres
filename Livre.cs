317bcffdeab24907a7898504a4c65ee86948dab6
Rachad <300140175@monboreal.ca>
2024-10-13 22:58:38 -0400

    Ajout du fichier Livre.cs

---
+++ Livre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCsharp_Roman
+{
+    internal class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+        // Constructeur
+        public Livre(string titre, string auteur, int nombrePages)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NombrePages = nombrePages;
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NombrePages}");
+        }
+    }
+}
