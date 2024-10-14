3e3c2b1e3fed638d70a78039ed85ec5ee7269e6e
Rachad <300140175@monboreal.ca>
2024-10-13 23:15:32 -0400

    Ajout du fichier classe Revue.cs

---
+++ Revue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCsharp_Roman
+{
+    internal class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+      
+        public Revue(string titre, string auteur, int nombrePages, int numero, int annee) : base(titre, auteur, nombrePages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NombrePages}, Numéro: {Numero}, Année: {Annee}");
+        }
+    }
+}
